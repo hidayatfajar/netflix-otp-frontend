4de7467f9c80342dd4133039d32315c08fcea40a
hidayatfajar <fajarnh67@gmail.com>
2025-03-01 22:19:51 +0700

    fix: update base path in Vite configuration for production

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ command }) => {
   const isProd = command === 'build'
 
   return {
-    base: isProd ? basenameProd : '',
+    base: isProd ? basenameProd : '/',
     plugins: [react()],
     resolve: {
       alias: {
